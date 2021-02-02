e99d6b3a993e9d18da55d7630a7b0eda7a877411
lukek <lukeklegraefe@gmail.com>
2021-02-02 16:46:48 -0500

    Palindromes with spaces are now accepted: i.e. race car = palindrome.

--- src/palindromeChecker.java
+++ src/palindromeChecker.java
@@ -4,13 +4,12 @@ public class palindromeChecker {
     public static void main(String[] args){
         Scanner stream = new Scanner(System.in);
         String input = "";
-        boolean p;
 
         while(!input.equals("Q")){
             System.out.print("\nPlease enter the word you would like to check (Q to exit): ");
             input = stream.nextLine();
             if(!input.equals("Q")) {
-                p = check(input);
+                check(input);
             }
             else{
                 System.exit(0);
@@ -18,20 +17,23 @@ public class palindromeChecker {
         }
     }
 
-    public static Boolean check(String input){
+    public static void check(String input){
+        String formattedInput = input.replaceAll("\\s+","");
         int i = 0;                                                                                          //set the first char to 0th position
-        int j = input.length();                                                                             //last char to last position
+        int j = formattedInput.length();                                                                             //last char to last position
+        boolean pal = true;
         while(i < j) {                                                                                      //run until i & j meet
-            if (Character.toLowerCase(input.charAt(i)) != Character.toLowerCase(input.charAt(j - 1))) {     //checks the two chars, disregarding case
-                System.out.println(input + " is not a palindrome. First mismatch is '" + input.charAt(i) +
-                        "' (position " + i + ") and '" + input.charAt(j - 1) + "' (position " + j + ")");
-
-                return false;                                                                               //break if any one of the chars do not match
+            if (Character.toLowerCase(formattedInput.charAt(i)) != Character.toLowerCase(formattedInput.charAt(j - 1))) {     //checks the two chars, disregarding case
+                System.out.println(input + " is not a palindrome. First mismatch is '" + formattedInput.charAt(i) +
+                        "' (position " + i + ") and '" + formattedInput.charAt(j - 1) + "' (position " + j + ")");
+                pal = false;
+                break;
             }
             i++;                                                                                            //step i and j respectively
             j--;
         }
-        System.out.println(input + " is a palindrome.");
-        return true;                                                                                        //otherwise, everything matches
+        if(pal){
+            System.out.println(input + " is a palindrome.");
+        }
     }
 }
